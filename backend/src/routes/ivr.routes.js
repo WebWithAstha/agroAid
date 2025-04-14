0b2f69acddf62c24714a56b51439651e72cac40c
WebWithAstha <lodhiastha85@gmail.com>
2025-04-14 16:50:48 +0530

    fixed

--- backend/src/routes/ivr.routes.js
+++ backend/src/routes/ivr.routes.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import { handleInput, handleRecording, processMessage, selectLanguage, startCall, voiceMenu } from '../controllers/ivr.controller.js';
+import {processMessage, selectLanguage, startCall, voiceMenu } from '../controllers/ivr.controller.js';
 const router = express.Router()
 
 
 router.get('/start-call', startCall);
-app.post("/ivr/voice-menu", voiceMenu);
-app.post("/ivr/select-language", selectLanguage);
-app.post("/ivr/process-message", processMessage);
+router.post("/ivr/voice-menu", voiceMenu);
+router.post("/ivr/select-language", selectLanguage);
+router.post("/ivr/process-message", processMessage);
 
 
 export default router;
