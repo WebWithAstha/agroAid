910e8e1abeb86c7be6b2f75f6340017268ae461b
webwithrathor <devrajrathor143@gmail.com>
2025-04-17 14:12:14 +0530

    .updated

---
+++ client/src/Services/weatherService.js
@@ -0,0 +1,9 @@
+import { Axios } from "../utils/axios";
+
+
+export const dailyWeatherForecastService = async () => {
+    const { data } = await Axios.get("/services/weather");
+    console.log(data);
+    return data;
+    
+}
