a8219956791ffe670a3b4dfa917400231b62e56f
WebWithAstha <lodhiastha85@gmail.com>
2025-04-22 11:21:37 +0530

    cookie samesite lax

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -46,14 +46,14 @@ export const setCookies = async (res, accessToken, refreshToken) => {
     res.cookie("accessToken", accessToken, {
         httpOnly: true,
         secure: true,
-        sameSite: "Strict",
+        sameSite: "Lax",
         maxAge: Number(config.JWT_COOKIE_EXPIRES_IN),
     });
 
     res.cookie("refreshToken", refreshToken, {
         httpOnly: true,
         secure: true,
-        sameSite: "Strict",
+        sameSite: "Lax",
         maxAge: Number(config.REFRESH_COOKIE_EXPIRES_IN),
     });
 };
